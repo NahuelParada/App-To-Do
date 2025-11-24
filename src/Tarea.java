22bc9b58819f8390c1e9d8acfffa65b4cc211ca7
Nahuel <paradacolo0@gmail.com>
2025-11-24 20:38:44 -0300

    Creacion clase Tarea con setters de todos los atributos y setter de completada con su ToString

---
+++ src/Tarea.java
@@ -0,0 +1,34 @@
+public class Tarea {
+
+    private int id;
+    private String descripcion;
+    private boolean completada;
+
+    public Tarea(int id, String descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.completada = false;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + (completada ? "X" : " ") + "] " + id + ": " + descripcion;
+    }
+}
